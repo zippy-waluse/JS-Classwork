4994c49474c21dd4630506a49222be679b0fe6a9
Zippy-Waluse <zippywaluse@gmail.com>
2024-05-17 23:23:49 +0300

    added file

--- dom.js
+++ dom.js
@@ -10,3 +10,33 @@ console.log('lastChild',container.lastElementChild);
 const html = document.documentElement;
 console.log({html});
 console.log('body',document.body);
+
+
+// UPDATING DOM
+const heading = document.getElementById('heading');
+heading.innerHTML = 'Introduction to DOM Manipulation';
+heading.style.color = 'red'
+
+// CREATE DOM
+const textChild = document.createElement('p');
+container.appendChild(textChild);
+textChild.innerHTML = 'This is the new child';
+textChild.setAttribute("Class",'container-new-child');
+textChild.setAttribute("id",'paragraph');
+
+// REMOVE DOM
+heading.remove();
+
+
+const button = document.getElementById('button');
+button.style.border = 'none';
+button.style.padding = '10px 15px';
+button.style.borderRadius = "5px"
+button.style.cursor = 'pointer';
+button.style.background = '#0096FF';
+button.style.color = '#ffffff';
+
+button.addEventListener('click',function(){
+    button.innerHTML = 'Clicked'
+    button.style.background = "#50C878"
+})
